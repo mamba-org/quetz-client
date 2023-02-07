7e70b1143be859ec37574c03793e419aba52e414
Simon Bohnen <simonmbohnen@gmail.com>
2023-02-07 22:40:59 +0000

    fixes

--- .github/workflows/test-readme.sh
+++ .github/workflows/test-readme.sh
@@ -15,4 +15,4 @@ bash ./quetz/download-test-package.sh
 
 quetz-client post_file_to_channel channel0 xtensor/linux-64/xtensor-0.16.1-0.tar.bz2 # xtensor/osx-64/xtensor-0.16.1-0.tar.bz2
 
-mamba install --override-channels -c http://localhost:8000/get/channel0 -y xtensor
+micromamba install --override-channels -c http://localhost:8000/get/channel0 -y xtensor
