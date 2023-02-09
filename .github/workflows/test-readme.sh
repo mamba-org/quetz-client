b74bebaac7725763f4282dfa6ec1f78ef088d7f7
Simon Bohnen <simonmbohnen@gmail.com>
2023-02-09 21:33:42 +0000

    now it should work?

--- .github/workflows/test-readme.sh
+++ .github/workflows/test-readme.sh
@@ -1,15 +1,15 @@
 set -e
 
-git clone https://github.com/mamba-org/quetz.git
+git clone https://github.com/simonbohnen/quetz.git
 
 pip install -e ./quetz-client
-micromamba install quetz
+pip install -e ./quetz
 micromamba install -y sqlalchemy=1.4.46
 
 quetz run test_quetz --copy-conf ./quetz/dev_config.toml --dev --reload > quetz.log &
 sleep 15
 export QUETZ_API_KEY=$(sed -n 's/.*key created for user.*: \(.*\)/\1/p' quetz.log)
-echo PRINTING API KEY
+echo "PRINTING API KEY"
 echo $QUETZ_API_KEY
 export QUETZ_SERVER_URL=http://localhost:8000
 
