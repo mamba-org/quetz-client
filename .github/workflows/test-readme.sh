0647d29efe0cc9ace13a222ec4c8da5559b1de03
Simon Bohnen <simonmbohnen@gmail.com>
2023-02-10 10:44:18 +0000

    remove debug prints

--- .github/workflows/test-readme.sh
+++ .github/workflows/test-readme.sh
@@ -10,21 +10,14 @@ pip install -e ./quetz
 quetz run test_quetz --copy-conf ./quetz/dev_config.toml --dev --reload > quetz.log &
 sleep 10
 export QUETZ_API_KEY=$(sed -n 's/.*key created for user.*: \(.*\)/\1/p' quetz.log)
-echo "PRINTING API KEY"
-echo $QUETZ_API_KEY
+# echo "PRINTING API KEY"
+# echo $QUETZ_API_KEY
 export QUETZ_SERVER_URL=http://localhost:8000
 
 bash ./quetz/download-test-package.sh
 
 quetz-client post_files_to_channel channel0 xtensor/linux-64/xtensor-0.16.1-0.tar.bz2 # xtensor/osx-64/xtensor-0.16.1-0.tar.bz2
 
-# list files in test_quetz recursively
-find test_quetz -type f
-
-# print size of test_quetz/channels/channel0/linux-64/xtensor-0.16.1-0.tar.bz2
-du -h xtensor/linux-64/xtensor-0.16.1-0.tar.bz2
-du -h test_quetz/channels/channel0/linux-64/xtensor-0.16.1-0.tar.bz2
-
-sleep 5
+sleep 2
 
 micromamba install --override-channels --strict-channel-priority -c http://localhost:8000/get/channel0 -c conda-forge xtensor
