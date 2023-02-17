996cf41da52dee7d759feb8df96a7fcbb5416655
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2023-02-17 17:44:50 +0100

    Fix version.

--- src/quetz_client/__init__.py
+++ src/quetz_client/__init__.py
@@ -1,10 +1,4 @@
-import pkg_resources
-
+from ._version import __version__
 from .client import QuetzClient
 
-try:
-    __version__ = pkg_resources.get_distribution(__name__).version
-except Exception:
-    __version__ = "unknown"
-
-__all__ = ("QuetzClient",)
+__all__ = ("QuetzClient", "__version__")
