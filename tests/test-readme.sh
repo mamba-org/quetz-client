6a5b08714b5836d03d880fbf5255905b74268090
Simon Bohnen <simonmbohnen@gmail.com>
2023-02-17 22:12:47 +0000

    add cleanup steps

--- tests/test-readme.sh
+++ tests/test-readme.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Run the steps described in the quetz README.md for uploading and installing a sample package
 
-quetz run test_quetz --copy-conf $SCRIPT_DIR/dev_config.toml --dev --reload > quetz.log &
+quetz run test_quetz --copy-conf $SCRIPT_DIR/dev_config.toml --dev --reload --delete > quetz.log &
 sleep 10
 export QUETZ_API_KEY=$(sed -n 's/.*key created for user.*: \(.*\)/\1/p' quetz.log)
 export QUETZ_SERVER_URL=http://localhost:8000
@@ -19,3 +19,6 @@ quetz-client post_file_to_channel channel0 xtensor/osx-64/xtensor-0.16.1-0.tar.b
 sleep 2
 
 micromamba install --override-channels --strict-channel-priority -c http://localhost:8000/get/channel0 -c conda-forge xtensor
+
+# Kill quetz
+lsof -i:8000 | grep LISTEN | awk '{print $2}' | xargs kill -9
